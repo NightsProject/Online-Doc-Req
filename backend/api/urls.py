a8d0cc1117358b381e1399e21b3d1172f51a39be
NightsProject <nightnightproject@gmail.com>
2025-10-02 12:45:11 +0800

    removed django and its dependencies

--- backend/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import patients_list
-
-urlpatterns = [
-    path("patients/", patients_list),
-]
--- backend/api/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .models import Patient
-
-@api_view(['GET'])
-def patients_list(request):
-    patients = Patient.objects.all().values('id', 'name', 'age', 'condition')
-    return Response(list(patients))
